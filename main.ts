93e3e4cdf96a2bf3539c483274710b7e7eb17efd
mark-lutabi <mark.lutabi@mths.ca>
2024-10-16 10:33:35 -0400

    changed comment, README.md and name

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark lutabi
+ * Created on: Oct 2024
+ * This program measures the distance from an object
 */
-
-basic.showString('Hello, World!')
