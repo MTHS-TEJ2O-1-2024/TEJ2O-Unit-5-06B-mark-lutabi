efe844167685e61a83a3b475243f11b87ff66d89
mark-lutabi <mark.lutabi@mths.ca>
2024-10-16 10:41:36 -0400

    finally distance to object code

--- main.ts
+++ main.ts
@@ -4,3 +4,19 @@
  * Created on: Oct 2024
  * This program measures the distance from an object
 */
+
+let distanceToObject:number = 0
+//setup
+basic.showIcon(IconNames.Happy)
+
+// find the distance of the sonar
+input.onButtonPressed(Button.A, function(){
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.clearScreen()
+})
